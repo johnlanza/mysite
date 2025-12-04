b088711fe9163d01c546842740781ffe69eedc0e
johnlanza <yRh6Bo#eEadm4hpr>
2025-12-04 11:37:12 -0800

    Add portrait header layout and responsive typography tweaks

--- app.js
+++ app.js
@@ -538,4 +538,5 @@ app.use((err, req, res, next) => {
 // -----------------------
 // Start Server
 // -----------------------
-app.listen(3000, () => console.log("Server running 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
