e15a71ec4bcc24fd00cd16009aec89a93048b78c
johnlanza <94123706+johnlanza@users.noreply.github.com>
2026-09-03 14:09:41 -0700

    Use approved Zette porcelain cameo icon

--- zette/src/lib/icon-config.ts
+++ zette/src/lib/icon-config.ts
@@ -1,4 +1,4 @@
-export const ZETTE_ICON_VERSION = "porcelain-cameo-20260903";
+export const ZETTE_ICON_VERSION = "porcelain-cameo-approved-20260903";
 export const ZETTE_ICON_THEME_COLOR = "#4a1f4e";
 export const ZETTE_ICON_BACKGROUND_COLOR = "#4a1f4e";
 
